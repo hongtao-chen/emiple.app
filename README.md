9ab5ae30966d81505871fbc832ada1b9f35e67ad
Hongtao Chen <20146794+hongtao-chen@users.noreply.github.com>
2025-11-24 12:50:14 -0800

    Initialize the repo with an empty react native application.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # emiple.app
 emiple mobile application
+
+# Debug
+
+```SH
+npm run android
+```
+
+
+## Release Build
+```SH
+cd android
+gradlew assembleRelease
+```
---
+++ android/app/src/main/java/com/emiple/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.emiple
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+  override fun getMainComponentName(): String = "Emiple"
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
