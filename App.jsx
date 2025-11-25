cba80b8932857279a4e4d5b51c3ff0ff20a6d426
Hongtao Chen <20146794+hongtao-chen@users.noreply.github.com>
2025-11-24 16:07:54 -0800

    added paper component lib.

--- App.jsx
+++ App.jsx
@@ -1,32 +1,53 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
     StatusBar,
     StyleSheet,
     useColorScheme,
     View,
-    Button,
     Linking
 } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+    PaperProvider,
+    MD3DarkTheme,
+    MD3LightTheme,
+    Text,
+    Button,
+    useTheme
+} from 'react-native-paper';
 
 function App() {
     const isDarkMode = useColorScheme() === 'dark';
+    const paperTheme = isDarkMode ? MD3DarkTheme : MD3LightTheme;
 
     return (
         <SafeAreaProvider>
-            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-            <AppContent />
+            <PaperProvider theme={paperTheme}>
+                <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+                <AppContent />
+            </PaperProvider>
         </SafeAreaProvider>
     );
 }
 
 function AppContent() {
+    const theme = useTheme();
+
     const openLink = () => {
         Linking.openURL('https://google.com');
     };
 
     return (
-        <View style={styles.container}>
-            <Button title="Open Chrome" onPress={openLink} />
+        <View style={[
+            styles.container,
+            { backgroundColor: theme.colors.background }
+        ]}>
+            <Text variant="titleLarge">Card Title</Text>
+
+            <Text variant="bodyMedium">
+              This text now adapts to system settings.
+            </Text>
+
+            <Button mode="contained" onPress={openLink}>Paper Button</Button>
         </View>
     );
 }
@@ -34,7 +55,7 @@ function AppContent() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
+    }
 });
 
 export default App;
